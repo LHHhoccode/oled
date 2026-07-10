8da14889869298d2854060afbe71fd4c09132760
LHHhoccode <lamhoanghung28112008@gmail.com>
2026-07-10 22:15:42 +0700

    Add files via upload

---
+++ oled_1.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin(8, 9);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("Hello Hung");
+  display.setCursor(10,10);
+  display.println("Arduino");
+  display.display();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
